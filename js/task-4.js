047ab6c57377736b6cfa4fa8afa51cdc08a7aedc
Mr_Whoopee <artem.holovko.97@gmail.com>
2025-10-29 19:16:14 +0300

    Done

---
+++ js/task-4.js
@@ -0,0 +1,52 @@
+// використав деструктуризацію з практики, тому замінив вхідну змінну на chooseGender
+// const getTotalBalanceByGender = (users, chooseGender) =>
+//   users
+//     .filter(({ gender }) => gender === chooseGender)
+//     .reduce((total, { balance }) => total + balance, 0);
+
+const getTotalBalanceByGender = (users, gender) =>
+  users
+    .filter(users => users.gender === gender)
+    .reduce((total, { balance }) => total + balance, 0);
+
+const clients = [
+  {
+    name: 'Moore Hensley',
+    gender: 'male',
+    balance: 2811,
+  },
+  {
+    name: 'Sharlene Bush',
+    gender: 'female',
+    balance: 3821,
+  },
+  {
+    name: 'Ross Vazquez',
+    gender: 'male',
+    balance: 3793,
+  },
+  {
+    name: 'Elma Head',
+    gender: 'female',
+    balance: 2278,
+  },
+  {
+    name: 'Carey Barr',
+    gender: 'male',
+    balance: 3951,
+  },
+  {
+    name: 'Blackburn Dotson',
+    gender: 'male',
+    balance: 1498,
+  },
+  {
+    name: 'Sheree Anthony',
+    gender: 'female',
+    balance: 2764,
+  },
+];
+
+console.log(getTotalBalanceByGender(clients, 'male')); // 12053
+
+console.log(getTotalBalanceByGender(clients, 'female')); // 8863
